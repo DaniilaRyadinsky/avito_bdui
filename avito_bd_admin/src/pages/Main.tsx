07447f3727e7feaa61a0d1ecbb1f215c6d3af382
DaniilaRyadinsky <130170619+DaniilaRyadinsky@users.noreply.github.com>
2025-10-01 13:20:28 +0300

    add screen

--- avito_bd_admin/src/pages/Main.tsx
+++ avito_bd_admin/src/pages/Main.tsx
@@ -1,17 +1,33 @@
 
+import { useState } from 'react'
 import LeftBar from '../features/LeftBar/ui/LeftBar'
 import { DemoPanel } from '../features/PropertyPanel/ui/DemoPanel'
 import { PropertyPanel } from '../features/PropertyPanel/ui/PropertyPanel'
+import { NumberInput } from '../shared/ui/NumberInput/NumberInput'
 import Topbar from '../widgets/Topbar/Topbar'
 import styles from './Main.module.css'
 
 const Main = () => {
+    const [width, setWidth] = useState(320);
+    const [height, setHeight] = useState(600);
+
     return (
         <div className={styles.container}>
             <Topbar />
             <div className={styles.main_window}>
-                <LeftBar/>
-                <DemoPanel/>
+                <LeftBar />
+                <div className={styles.work_panel}>
+                    <div className={styles.work_panel_top}>
+                        <div className={styles.input_container}>
+                            <NumberInput value={width} min={0} max={2000} onChange={(e) => setWidth(e)} />
+                            <NumberInput value={height} min={0} max={2000} onChange={(e) => setHeight(e)} />
+                        </div>
+                    </div>
+                    <div className={styles.workspace} >
+                        <div className={styles.screen} style={{ width: width, height: height }}></div>
+                    </div>
+                </div>
+                <DemoPanel />
             </div>
 
         </div>
